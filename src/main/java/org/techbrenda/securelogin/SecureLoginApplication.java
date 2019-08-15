af701ac40919e61c0f257a5c9c289e0f6d3711c2
TechBrenda <tech.brenda@gmail.com>
2019-08-15 12:44:38 -0500

    Initial commit

---
+++ src/main/java/org/techbrenda/securelogin/SecureLoginApplication.java
@@ -0,0 +1,13 @@
+package org.techbrenda.securelogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureLoginApplication.class, args);
+	}
+
+}
