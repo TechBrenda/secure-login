ae4715000ccb4fcf50185d3c579813b48092c04d
TechBrenda <tech.brenda@gmail.com>
2019-08-24 22:06:34 -0500

    Configure user details

--- src/main/java/org/techbrenda/securelogin/auth/SecurityConfiguration.java
+++ src/main/java/org/techbrenda/securelogin/auth/SecurityConfiguration.java
@@ -1,14 +1,46 @@
 package org.techbrenda.securelogin.auth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
   
+  @Autowired
+  private UserDetailsService userDetailsService;
+  
+  @Bean
+  public BCryptPasswordEncoder bCryptPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
+  @Override
+  public void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+  }
+  
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+  
   @Override
   protected void configure(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .csrf().disable();
+    
+    httpSecurity
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        
     httpSecurity
         .authorizeRequests().antMatchers("/").permitAll()
         .and()
@@ -16,11 +48,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         .and()
         .authorizeRequests().antMatchers("/authenticate", "/register").permitAll()
         .and()
-        .authorizeRequests().antMatchers("/refresh").hasAnyRole("USER", "ADMIN");
+        .authorizeRequests().antMatchers("/refresh").authenticated()
+        .and()
+        .authorizeRequests().anyRequest().authenticated();
     
-    httpSecurity
-        .csrf().disable();
-        
     httpSecurity
         .headers().frameOptions().sameOrigin();
   }
--- src/main/java/org/techbrenda/securelogin/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/org/techbrenda/securelogin/auth/service/UserDetailsServiceImpl.java
@@ -1,11 +1,17 @@
 package org.techbrenda.securelogin.auth.service;
 
+import java.util.HashSet;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.techbrenda.securelogin.auth.model.Authority;
 import org.techbrenda.securelogin.auth.model.UserAccount;
 import org.techbrenda.securelogin.auth.repository.UserAccountRepository;
 
@@ -19,8 +25,22 @@ public class UserDetailsServiceImpl implements UserDetailsService {
   @Transactional
   public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
     UserAccount userAccount = userAccountRepo.findByEmail(email);
+    if (userAccount == null) {
+      throw new UsernameNotFoundException(email);
+    }
     
-		return null;
+    Set<GrantedAuthority> authorities = new HashSet<>();
+    for (Authority authority : userAccount.getAuthorities()) {
+      authorities.add(new SimpleGrantedAuthority(authority.getRole()));
+    }
+    
+    return new User(userAccount.getEmail(),
+                userAccount.getPassword(),
+                userAccount.getEnabled(),
+                userAccount.getAccountNonExpired(),
+                userAccount.getCredentialsNonExpired(),
+                userAccount.getAccountNonLocked(),
+                authorities);
 	}
   
 }
