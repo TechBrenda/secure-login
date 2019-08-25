c167f35a2b3db5969b1e22f0aa64f9735f9a8799
TechBrenda <tech.brenda@gmail.com>
2019-08-24 23:18:35 -0500

    Started authentication method.

--- src/main/java/org/techbrenda/securelogin/auth/controller/AuthController.java
+++ src/main/java/org/techbrenda/securelogin/auth/controller/AuthController.java
@@ -1,11 +1,19 @@
 package org.techbrenda.securelogin.auth.controller;
 
+import java.util.Objects;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
+  
+  @Autowired
+  private AuthenticationManager authenticationManager;
   
   @PostMapping("/authenticate")
   public void createAuthToken() {}
@@ -15,4 +23,17 @@ public class AuthController {
   
   @PostMapping("/register")
   public void registration() {}
+  
+  private void authenticate(String username, String password) {
+    Objects.requireNonNull(username);
+    Objects.requireNonNull(password);
+    
+    /* try {
+      authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+    } catch (DisabledException e) {
+      throw new AuthException("USER_DISABLED", e);
+    } catch (BadCredentialsException e) {
+      throw new AuthException("INVALID_CREDENTIALS", e);
+    } */
+  }
 }
